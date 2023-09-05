bc4cb397eb232bed35bbbd9acc2f7f42437513b1
josechanfm <josechan@ipm.edu.mo>
2023-09-05 17:06:24 +0800

    vite

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig(({ mode }) => {
+
+    process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+
+    return {
+        server: {
+            host: '0.0.0.0',
+            hmr: {
+                host: process.env.VITE_HOST ?? '127.0.0.1',
+            },
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.js',
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
+                },
+            }),
+        ],
+    }
+});
