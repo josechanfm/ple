91314356140134b6f1f931f2ea00845845b4b14c
josechanfm <josechan@ipm.edu.mo>
2023-09-08 20:01:52 +0800

    tansition

--- resources/js/Pages/Form/Form.vue
+++ resources/js/Pages/Form/Form.vue
@@ -6,6 +6,7 @@
                 Application Froms
             </h2>
         </template>
+
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg ">
                 <a-table :dataSource="forms" :columns="columns">
@@ -20,6 +21,30 @@
                 </a-table>
             </div>
         </div>
+
+
+        <a-steps :current="currentStep">
+            <a-step>
+            <!-- <span slot="title">Finished</span> -->
+            <template #title>Finished</template>
+            <template #description>
+                <span>This is a description.</span>
+            </template>
+            </a-step>
+            <a-step title="In Progress" sub-title="Left 00:00:08" description="This is a description." />
+            <a-step title="Waiting" description="This is a description." />
+        </a-steps>
+        <div v-if="currentStep==0" class="h-48 bg-sky-500">
+            <p>Page 0</p>
+        </div>
+        <div v-if="currentStep==1"  class="h-48 bg-orange-500 transition duration-700 ease-in opacity-100" :class="currentStep==1?'opacity-50':'opacity-0'">
+            <p>Page 1</p>
+        </div>
+        <div v-if="currentStep==2" class="h-48 bg-yellow-500 ">
+            <p>Page 2</p>
+        </div>
+        <a-button @click="currentStep--">previours</a-button>
+        <a-button @click="currentStep++">next</a-button>
     </WebLayout>
 
 </template>
@@ -35,6 +60,25 @@ export default {
     props: ['forms'],
     data() {
         return {
+            currentStep:1,
+            items:[
+                {
+                    title: 'Login',
+                    status: 'finish',
+                },
+                {
+                    title: 'Verification',
+                    status: 'finish',
+                },
+                {
+                    title: 'Pay',
+                    status: 'process',
+                },
+                {
+                    title: 'Done',
+                    status: 'wait',
+                },
+            ],
             columns:[
                 {
                     title: 'Name',
@@ -91,3 +135,11 @@ export default {
     },
 }
 </script>
+
+
+<style>
+.fade-in-out {
+  opacity: 0;
+  animation: fade-in 1s ease-in forwards, fade-out 4s 1s ease-out forwards;
+}
+</style>
