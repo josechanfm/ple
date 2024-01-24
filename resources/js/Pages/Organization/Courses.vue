a379c8241650c39454ac0d0beaaffd1d1df7f5f1
Allen Tan <112374707+AllenWacr@users.noreply.github.com>
2024-01-24 17:29:16 +0800

    2024-01-24 minor patch (3) BUG WARN
    DO NOT CREATE COURSE XD

--- resources/js/Pages/Organization/Courses.vue
+++ resources/js/Pages/Organization/Courses.vue
@@ -44,6 +44,33 @@
         <a-form-item label="Title" name="title">
           <a-input v-model:value="createModal.data.title" />
         </a-form-item>
+
+        <a-form ref="formRef" name="dynamic_form_item" :model="dynamicValidateForm" v-bind="formItemLayoutWithOutLabel">
+          <a-form-item v-for="(domain, index) in dynamicValidateForm.domains" :key="domain.key"
+            v-bind="index === 0 ? formItemLayout : {}" :label="index === 0 ? 'Domains' : ''"
+            :name="['domains', index, 'value']" :rules="{
+              required: true,
+              message: 'domain can not be null',
+              trigger: 'change',
+            }">
+            <a-input v-model:value="domain.value" placeholder="please input domain"
+              style="width: 60%; margin-right: 8px" />
+            <MinusCircleOutlined v-if="dynamicValidateForm.domains.length > 1" class="dynamic-delete-button"
+              @click="removeDomain(domain)" />
+          </a-form-item>
+          <a-form-item v-bind="formItemLayoutWithOutLabel">
+            <a-button type="dashed" style="width: 60%" @click="addDomain">
+              <PlusOutlined />
+              Add field
+            </a-button>
+          </a-form-item>
+          <a-form-item v-bind="formItemLayoutWithOutLabel">
+            <a-button type="primary" html-type="submit" @click="submitForm">Submit</a-button>
+            <a-button style="margin-left: 10px" @click="resetForm">Reset</a-button>
+          </a-form-item>
+        </a-form>
+
+
         <a-form-item label="Learn" name="learn">
           <a-textarea v-model:value="createModal.data.learn" />
         </a-form-item>
@@ -206,5 +233,87 @@ export default {
     }
   }
 };
+
 </script>
-  
+<script setup>
+import { reactive, ref } from 'vue';
+import { MinusCircleOutlined } from '@ant-design/icons-vue';
+const formRef = ref();
+const formItemLayout = {
+  labelCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 4,
+    },
+  },
+  wrapperCol: {
+    xs: {
+      span: 24,
+    },
+    sm: {
+      span: 20,
+    },
+  },
+};
+const formItemLayoutWithOutLabel = {
+  wrapperCol: {
+    xs: {
+      span: 24,
+      offset: 0,
+    },
+    sm: {
+      span: 20,
+      offset: 4,
+    },
+  },
+};
+const dynamicValidateForm = reactive({
+  domains: [],
+});
+const submitForm = () => {
+  formRef.value
+    .validate()
+    .then(() => {
+      console.log('values', dynamicValidateForm.domains);
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
+};
+const resetForm = () => {
+  formRef.value.resetFields();
+};
+const removeDomain = item => {
+  const index = dynamicValidateForm.domains.indexOf(item);
+  if (index !== -1) {
+    dynamicValidateForm.domains.splice(index, 1);
+  }
+};
+const addDomain = () => {
+  dynamicValidateForm.domains.push({
+    value: '',
+    key: Date.now(),
+  });
+};
+</script>
+<style scoped>
+.dynamic-delete-button {
+  cursor: pointer;
+  position: relative;
+  top: 4px;
+  font-size: 24px;
+  color: #999;
+  transition: all 0.3s;
+}
+
+.dynamic-delete-button:hover {
+  color: #777;
+}
+
+.dynamic-delete-button[disabled] {
+  cursor: not-allowed;
+  opacity: 0.5;
+}
+</style>
